1bc4d53630f83ae5b373ff4e47df997cd15b599f
TreeJin99 <90708425+TreeJin99@users.noreply.github.com>
2022-07-04 23:23:31 +0900

    Update TodoViewModel.kt

--- app/src/main/java/com/example/mytodo/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/example/mytodo/viewmodel/TodoViewModel.kt
@@ -7,11 +7,19 @@ import androidx.lifecycle.asLiveData
 import com.example.mytodo.database.TodoDatabase
 import com.example.mytodo.dto.TodoModel
 import com.example.mytodo.repository.TodoRepository
-
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+
+/**
+ * TODO
+ * MVVM 기준으로 봤을때 AndroidViewModel은 적절하지 않다.
+ * Database에 대한 의존성 주입을 다시 한번 고민해보기
+ * Application을 넣어주는 주체가 todoViewModel이 되면 안된다.
+ *
+ */
+
 
 class TodoViewModel(application: Application): AndroidViewModel(application) {
     private val todoDao = TodoDatabase.getDatabase(application)!!.todoDao()
